f04d459a53e6249322d10b6f19ac0b681b670a0d
Esaou <eric.saou3@gmail.com>
2021-11-21 19:48:28 +0100

    refactor HomeController as AccueilController

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function home(): Response
+    #[Route('/', name: 'accueil')]
+    public function accueil(): Response
     {
-        return $this->render('home/home.html.twig', [
+        return $this->render('accueil/accueil.html.twig', [
 
         ]);
     }
